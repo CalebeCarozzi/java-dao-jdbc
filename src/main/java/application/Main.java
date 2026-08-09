d0a51b0b9e052c88e40cd0dff925a8153c484a77
Calebe Carozzi <calebercarozzi@outlook.com>
2026-08-09 00:32:06 -0300

    feat: adicionando estrutura inicial

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/db/DbIntegriryException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntegriryException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DbIntegriryException(String message) {
+        super(message);
+    }
+
+}
