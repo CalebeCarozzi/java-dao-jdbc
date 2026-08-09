86e602da5f333425191399ee9965ccf8c77edcc5
Calebe Carozzi <calebercarozzi@outlook.com>
2026-08-09 15:23:58 -0300

    feat: implementacao do findAll

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -20,10 +20,13 @@ public class Main {
 
         System.out.println("==== TESTE 2: seller findByDepartment ====");
         Department department = new Department(2, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        list.forEach(System.out::println);
+        List<Seller> list1 = sellerDao.findByDepartment(department);
+        list1.forEach(System.out::println);
         System.out.println(department);
 
+        System.out.println("==== TESTE 3: seller findAll ====");
+        List<Seller> list2 = sellerDao.findAll();
+        list2.forEach(System.out::println);
     }
 }
 
--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -93,7 +93,40 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+
+            st = conn.prepareStatement(
+                    "select s.*, d.name as DepName " +
+                            "from seller s inner join department d " +
+                            "on s.DepartmentId = d.id " +
+                            "order by Name ");
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                if(dep == null){
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
