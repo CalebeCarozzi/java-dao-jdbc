76b27d958034ad0ac667352055b146a42854fe9b
Calebe Carozzi <calebercarozzi@outlook.com>
2026-08-09 12:29:03 -0300

    refactor: separando em funcoes o findById

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,17 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Main {
     public static void main(String[] args) {
+
+        SellerDao sellerDao = DaoFactory.crateSellerDao();
+
+        System.out.println("==== TESTE 1: seller findById ====");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
     }
 }
